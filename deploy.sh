6cbb7326e8d635509ade6128f74b35dd358068e1
Wei Tao <TaoWe@ap.leaseplancorp.net>
2020-01-02 14:01:03 +1100

    change a configuration

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ echo "_______________________________"
 npm install -g serverless
 npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 
-echo "Deploying to $env"
+echo "Deploying to $env with codebuild src dir $CODEBUILD_SRC_DIR"
 echo "_______________________________"
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
